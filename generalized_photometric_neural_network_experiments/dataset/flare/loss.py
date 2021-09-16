75578338ba92b03ea8178c1510601e320c0da75a
Greg Olmschenk <greg@olmschenk.com>
2021-09-16 16:50:16 -0400

    Add thresholded absolute difference calculations for the flares

---
+++ generalized_photometric_neural_network_experiments/dataset/flare/loss.py
@@ -0,0 +1,25 @@
+from tensorflow.python.keras.losses import MeanSquaredError
+from tensorflow.python.keras import backend
+import tensorflow as tf
+from tensorflow.python.ops import math_ops
+
+
+class FlareThresholdMeanSquaredErrorCalculator:
+    def __init__(self, slope_threshold: float, intercept_threshold: float):
+        self.slope_threshold = slope_threshold
+        self.intercept_threshold = intercept_threshold
+        self.threshold = tf.constant([[self.slope_threshold, self.intercept_threshold]], dtype=tf.float32)
+
+    def thresholded_absolute_difference(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+        """
+        Calculates the absolute difference with true NaN values being the difference above a given threshold.
+
+        :param y_true: The true values, with NaNs being calculated based on the difference above a threshold.
+        :param y_pred: The predicted values.
+        :return: The differences.
+        """
+        over_threshold_difference = backend.maximum(y_pred - self.threshold, tf.constant(0, dtype=tf.float32))
+        threshold_condition = math_ops.is_nan(y_true)
+        absolute_difference = math_ops.abs(y_pred - y_true)
+        difference = tf.where(threshold_condition, over_threshold_difference, absolute_difference)
+        return difference
---
+++ tests/dataset/flare/test_loss.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+
+from generalized_photometric_neural_network_experiments.dataset.flare.loss import \
+    FlareThresholdMeanSquaredErrorCalculator
+
+
+class TestFlareThresholdMeanSquaredErrorCalculator:
+    def test_absolute_difference_with_unthresholded_values(self):
+        y_true = tf.convert_to_tensor([[0, 1], [0, 4]], dtype=tf.float32)
+        y_pred = tf.convert_to_tensor([[0, 2], [2, 1]], dtype=tf.float32)
+        error_calculator = FlareThresholdMeanSquaredErrorCalculator(slope_threshold=-1, intercept_threshold=-1)
+        difference = error_calculator.thresholded_absolute_difference(y_true, y_pred)
+        expected_difference = np.array([[0, 1], [2, 3]])
+        assert np.allclose(difference, expected_difference)
+
+    def test_absolute_difference_with_thresholded_values(self):
+        y_true = tf.convert_to_tensor([[5, 5], [np.nan, np.nan], [np.nan, np.nan]], dtype=tf.float32)
+        y_pred = tf.convert_to_tensor([[3, 4], [2, 2], [4, 4]], dtype=tf.float32)
+        error_calculator = FlareThresholdMeanSquaredErrorCalculator(slope_threshold=2, intercept_threshold=3)
+        difference = error_calculator.thresholded_absolute_difference(y_true, y_pred)
+        expected_difference = np.array([[2, 1], [0, 0], [2, 1]])
+        assert np.allclose(difference, expected_difference)
