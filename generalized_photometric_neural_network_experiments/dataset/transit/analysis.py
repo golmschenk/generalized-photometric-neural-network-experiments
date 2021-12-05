8e7305c8c83013600935dc0588b9c8195298b60f
Greg Olmschenk <greg@olmschenk.com>
2021-12-05 10:15:32 -0600

    Fix loss metrics using the new Keras API

--- generalized_photometric_neural_network_experiments/dataset/transit/analysis.py
+++ generalized_photometric_neural_network_experiments/dataset/transit/analysis.py
@@ -2,18 +2,25 @@ import numpy as np
 
 from generalized_photometric_neural_network_experiments.dataset.transit.light_curve_collection import \
     TransitExperimentLightCurveCollection
+from generalized_photometric_neural_network_experiments.dataset.transit.names_and_paths import TransitLabel
 
 
 def show_dataset_statistics():
-    collection = TransitExperimentLightCurveCollection()
-    light_curve_paths = collection.get_paths()
-    print(f'Number of light curves: {len(list(light_curve_paths))}')
-    light_curve_lengths = []
-    for light_curve_path in light_curve_paths:
-        times, fluxes = collection.load_times_and_fluxes_from_path(light_curve_path)
-        light_curve_lengths.append(times.shape[0])
-    print(f'Mean light curve length: {np.mean(light_curve_lengths)}')
-    print(f'Median light curve length: {np.median(light_curve_lengths)}')
+    for name, collection in {'combined': TransitExperimentLightCurveCollection(),
+                             'planet': TransitExperimentLightCurveCollection(label=TransitLabel.PLANET),
+                             'non_planet': TransitExperimentLightCurveCollection(label=TransitLabel.NON_PLANET)
+                             }.items():
+        print('#' * 30)
+        print(name)
+        print('-' * 30)
+        light_curve_paths = collection.get_paths()
+        print(f'Number of light curves: {len(list(light_curve_paths))}')
+        light_curve_lengths = []
+        for light_curve_path in light_curve_paths:
+            times, fluxes = collection.load_times_and_fluxes_from_path(light_curve_path)
+            light_curve_lengths.append(times.shape[0])
+        print(f'Mean light curve length: {np.mean(light_curve_lengths)}')
+        print(f'Median light curve length: {np.median(light_curve_lengths)}')
 
 
 if __name__ == '__main__':
