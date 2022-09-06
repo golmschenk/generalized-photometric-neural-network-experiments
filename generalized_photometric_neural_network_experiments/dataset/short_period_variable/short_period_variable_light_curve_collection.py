4a5c9cb875bbba956c39c356684ddf71eb9558cf
Greg Olmschenk <greg@olmschenk.com>
2022-09-06 20:52:11 +0200

    Add uniform noise synthetic dataset and correct bug in times of sine wave dataset

--- generalized_photometric_neural_network_experiments/dataset/short_period_variable/short_period_variable_light_curve_collection.py
+++ generalized_photometric_neural_network_experiments/dataset/short_period_variable/short_period_variable_light_curve_collection.py
@@ -31,6 +31,28 @@ class SineWaveLightCurveCollection(TessFfiLightCurveCollection):
         max_amplitude = 1
         min_amplitude = 0.001
         amplitude = np.random.uniform(min_amplitude, max_amplitude)
-        times = np.linspace(0, math.tau * periods_to_produce, num=light_curve_length, endpoint=False)
-        magnifications = (np.sin(times) * amplitude) + 1
+        phase = np.linspace(0, math.tau * periods_to_produce, num=light_curve_length, endpoint=False)
+        magnifications = (np.sin(phase) * amplitude) + 1
+        times = np.linspace(0, 30, num=light_curve_length, endpoint=False)
+        return times, magnifications
+
+
+class UniformNoiseLightCurveCollection(TessFfiLightCurveCollection):
+    def get_paths(self) -> Iterable[Path]:
+        return [Path('')]
+
+    def load_times_and_magnifications_from_path(self, path: Path) -> (np.ndarray, np.ndarray):
+        """
+        Loads the times and magnifications from a random generated signal.
+
+        :param path: empty path
+        :return: The times and the magnifications of the signal.
+        """
+        length__days = 30
+        light_curve_length = length__days * 24 * 4  # 15 minute intervals.
+        max_amplitude = 1
+        min_amplitude = 0.001
+        amplitude = np.random.uniform(min_amplitude, max_amplitude)
+        times = np.linspace(0, 30, num=light_curve_length, endpoint=False)
+        magnifications = np.random.uniform(low=1 - amplitude, high=1 + amplitude, size=times.shape)
         return times, magnifications
